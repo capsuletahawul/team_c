0e3ab40d0106ff6d7fd7a51e5084707f74cb5929
kimiiine <awjatone@gmail.com>
2026-07-05 07:41:56 +0300

    added the student dashboard webpage

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,22 +1,14 @@
 import { useState } from 'react'
 import StudentDashboard from './StudentDashboard'
-import StudentProfile from './StudentProfile'
-// import AdminDashboard from './AdminDashboard'
+
 
 function App() {
-  // 'dashboard' = first screen a Student sees after logging in
-  // 'profile'   = opened by clicking the profile picture on the dashboard
-  const [view, setView] = useState('dashboard')
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      {view === 'dashboard' && (
-        <StudentDashboard onNavigateToProfile={() => setView('profile')} />
-      )}
-      {view === 'profile' && (
-        <StudentProfile onBack={() => setView('dashboard')} />
-      )}
-    </>
+    <StudentDashboard/>
+         </>
   )
 }
 
