dfc4bcce911c226c32e76477bb350c1553307043
Reem <reemabuaziz25@gmail.com>
2026-07-05 18:52:33 +0300

    Add Trainer Details page

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,14 +2,13 @@ import { useState } from 'react'
 import LandingPage from './LandingPage'
 import AdminDashboard from './AdminDashboard'
 
-
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <LandingPage/>
-         </>
+      <LandingPage />
+    </>
   )
 }
 
