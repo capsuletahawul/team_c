76a815fda1559b2482c4b30b93a5f31de1e91e17
kimiiine <awjatone@gmail.com>
2026-07-05 07:55:12 +0300

    added the landing webpage

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import StudentProfile from './StudentProfile'
+import LandingPage from './LandingPage'
 
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-    <StudentProfile/>
+    <LandingPage/>
          </>
   )
 }
